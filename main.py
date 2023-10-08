f6b475e06c8a1589d90c82356d8190384db694c1
LT <larisa.turlova@gmail.com>
2023-10-08 20:37:54 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,45 @@
-from cats import Cats
+class Cat():
+    def __init__(self, breed, color, age):
+        self._breed = breed
+        self._color = color
+        self._age = age
+
+    def breed(self):
+        return self._breed
+
+    def color(self):
+        return self._color
+
+    def age(self):
+        return self._age
+
+    def age(self, new_age):
+        if new_age > self._age:
+            self._age = new_age
+        return self._age
 
 
-if __name__ == '__main__':
+cat1 = Cat('Сфинкс', 'черный', 5)
+cat2 = Cat('Герман-рекс', 'черепаховый', 2)
 
-    tomas = Cats(name="Tomas")
-    tomas.pipi()
+print(cat1.breed)
+print(cat2._color)
+print(cat1.age)
 
-    print(tomas)
-    print(tomas.get_hungry())
+
+class HomeCat(Cat):
+    def __init__(self, breed, color, age, master, name):
+        super().__init__(breed, color, age)
+        self._master = master
+        self._name = name
+
+    def master(self):
+        return self._master
+
+    def name(self):
+        return self._name
+
+cat3 = HomeCat('Сфинкс', 'розовый', 5, 'Лариса', 'Семён')
+
+print(cat3.master)
+print(cat3._name)
