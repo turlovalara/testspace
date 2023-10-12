b933d283303f0439e36516fd9f4106e253e6e348
LT <larisa.turlova@gmail.com>
2023-10-12 19:52:04 +0300

    Add files via upload

---
+++ cars/cars.py
@@ -0,0 +1,17 @@
+from random import choice
+
+
+class Cars:
+
+    def __init__(self, manufacturer, model):
+        self.manufacturer = manufacturer
+        self.model = model
+
+
+    def manufacturer(self):
+        return self._manufacturer
+
+    def model(self):
+        return self._model
+
+    def set_car_brakes():
