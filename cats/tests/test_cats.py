9ef1c2e0f7566ec617a9935bfc9a8a290dc513ea
LT <larisa.turlova@gmail.com>
2023-10-12 19:21:53 +0300

    Update test_cats.py

--- cats/tests/test_cats.py
+++ cats/tests/test_cats.py
@@ -22,3 +22,19 @@ def test_car_engine(generate_car):
     engine_status = car.set_engine()
 
     assert engine_status is False
+
+@pytest.mark.car
+    def test_car_brakes(generate_car):
+        car = generate_car
+        brakes_status = car.get_brakes()
+
+        assert brakes_status is False
+
+        brakes_status = car.drive()
+
+        assert brakes_status is True
+
+        brakes_status = car.stop()
+
+        assert brakes_status is False
+        
